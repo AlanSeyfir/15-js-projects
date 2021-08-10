acd18abeb1cb9c5493f1fbf0aa9e9e88c5da024c
AlanSeyfir <48397562+AlanSeyfir@users.noreply.github.com>
2021-08-10 16:48:19 -0500

    Finish 8 project

--- projects/08-project/app.js
+++ projects/08-project/app.js
@@ -1,4 +1,28 @@
+//using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) =>{
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', () =>{
+        questions.forEach((item) =>{
+            if (item !== question) {
+                item.classList.remove('show-text');
+            }
+        });
+        question.classList.toggle('show-text');
+    });    
+});
 
 
+// traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
 
-
+// btns.forEach((btn) => {
+//     btn.addEventListener('click', (e) =>{
+//         //Debug the DOM. Show in console the element when click
+//         console.log(e.currentTarget.parentElement.parentElement);
+//         const question = e.currentTarget.parentElement.parentElement;
+        
+//         question.classList.toggle('show-text');
+//     }); 
+// })
