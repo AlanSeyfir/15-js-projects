3d6df9499e1a73202d8dfd6c478bd53e1742cc35
AlanSeyfir <48397562+AlanSeyfir@users.noreply.github.com>
2021-08-12 18:27:54 -0500

    Finish 10 project and code cleanup

--- projects/10-project/app.js
+++ projects/10-project/app.js
@@ -6,6 +6,7 @@
 // ********** set date ************
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
+
 // ********** close links ************
 const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
@@ -24,10 +25,56 @@ navToggle.addEventListener('click', () =>{
 });
 
 // ********** fixed navbar ************
+const navbar = document.getElementById('nav');
+//const topLink = document.querySelector('.top-Link') //Don't why doesn't get the query
+const topLink = document.getElementById('topLink');
+
+window.addEventListener('scroll', () =>{
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+    }else{
+        navbar.classList.remove('fixed-nav');
+    }
+
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link');
+    }else{
+        topLink.classList.remove('show-link');
+    }
+});
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link) =>{
+    link.addEventListener('click', (e) =>{
+        //prevent default
+        e.preventDefault();
+        //navigate specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+        //calculate the heights
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed-nav');
+        let position = element.offsetTop - navHeight; //console.log(position);
 
+        if (!fixedNav) {
+            position = position - navHeight;
+        }
 
+        if (navHeight > 82) {
+            position = position + containerHeight;
+        }
+
+        window.scrollTo({
+            left:0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
+});
 
 
