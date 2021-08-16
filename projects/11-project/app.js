04de2fcc29852b5bc370755ff9c82da3d381eb0a
AlanSeyfir <48397562+AlanSeyfir@users.noreply.github.com>
2021-08-16 17:23:55 -0500

    Finish 11 project

--- projects/11-project/app.js
+++ projects/11-project/app.js
@@ -1,4 +1,24 @@
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll('.content');
 
+//data-id
+about.addEventListener('click', (e) =>{
+    const id = e.target.dataset.id;
+    console.log(id);
+    if (id) {
+        //remove active from other buttons
+        btns.forEach((btn) =>{
+            btn.classList.remove('active');
+            e.target.classList.add('active');
+        });
 
-
-
+        //hide other articles
+        articles.forEach((article) =>{
+            article.classList.remove('active');
+        });
+        const element = document.getElementById(id);
+        element.classList.add('active');
+    }
+    //console.log(e.target);
+});
